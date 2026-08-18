a5ec4ce36a1044e19992f34f70648dee44f74fa1
Felipe Olivato Coimbra <felipe.olicoimbra@gmail.com>
2026-08-17 21:51:03 -0300

    Select MonoalphabeticCrypt added

--- Chat Socket TCP/src/main/java/utils/Utils.java
+++ Chat Socket TCP/src/main/java/utils/Utils.java
@@ -6,14 +6,13 @@ import java.util.regex.Pattern;
 public class Utils {
 
     public static String cleanString(String s) {
-//        upperString = s.toUpperCase();
-//        stringC = s.replace("Ç", "C");
-//        normalizedString = Normalizer.normalize(strm Normalizer.Form.NFD);
-//        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
-//        cleanString = padrao.matcher(normalizada).replaceAll("");
-//
-//        return cleanString;
-        return null;
+        String upperString = s.toUpperCase();
+        String stringC = s.replace("Ç", "C");
+        String normalizedString = Normalizer.normalize(s, Normalizer.Form.NFD);
+        Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
+        String cleanString = pattern.matcher(normalizedString).replaceAll("");
+
+        return cleanString;
     }
 
 }
